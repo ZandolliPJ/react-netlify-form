15bf0a1fd228fe80fdd6c84b4505c705110de60e
ZandolliPJ <judithpascal07@gmail.com>
2024-10-28 22:35:58 +0100

    add contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,28 @@ import React from "react";
 import "./ContactForm.css";
 
 function ContactForm() {
-  return <div>form</div>;
+  return (
+    <div className="contact">
+      <form
+        name="contact"
+        method="POST"
+        className="contact_form"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <label for="name">Nom: </label>
+        <input type="text" name="name" />
+
+        <label for="email">Email: </label>
+        <input type="email" name="email" />
+
+        <label for="nmessage">Message:</label>
+        <textarea name="message"></textarea>
+
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
